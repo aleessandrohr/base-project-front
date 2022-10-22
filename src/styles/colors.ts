deb0716b69a4afb7e680172785911fcb6adc37c1
Alessandro H. Ramos <70819804+aleessandrohr@users.noreply.github.com>
2022-10-22 10:08:45 -0300

    Initial commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+export const COLORS = {
+	root: css`
+		--primary: #01d2ce;
+		--black: #000000;
+		--white: #ffffff;
+		--shadow: #00000050;
+		--scrollbar: var(--primary);
+	`,
+	dark: css`
+		--gray: #e1e1e6;
+		--gray-dark: #a8a8b3;
+		--background: #121212;
+		--text: var(--gray);
+		--text-dark: var(--gray-dark);
+		--outline-color: var(--gray);
+		--scrollbar-background: #00000050;
+	`,
+};
